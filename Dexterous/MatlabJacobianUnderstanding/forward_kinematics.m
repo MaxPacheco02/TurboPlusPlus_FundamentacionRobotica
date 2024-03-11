ce27545ac227138f3496ab42ad3b7626799b1c4c
MaxPacheco02 <a01552369@tec.mx>
2024-03-11 03:21:17 -0600

    changes huh

--- Dexterous/MatlabJacobianUnderstanding/forward_kinematics.m
+++ Dexterous/MatlabJacobianUnderstanding/forward_kinematics.m
@@ -17,20 +17,20 @@ grid on
 i = 0.3;
 p = [0,0,0]; % Positions of joints 
 t1 = i;
-t2 = -pi/4;
-t3 = -pi/4;
+t2 = pi/4;
+t3 = pi/4;
 % Matrixes for rotation in axes of joints
 rot1 = [cos(t1), -sin(t1), 0;
     sin(t1), cos(t1), 0;
     0, 0, 1;
 ];
-rot2 = [cos(t2), 0, sin(t2);
+rot2 = [cos(t2), 0, -sin(t2);
     0, 1, 0;
-    -sin(t2), 0, cos(t2);
+    sin(t2), 0, cos(t2);
 ];
-rot3 = [cos(t3), 0, sin(t3);
+rot3 = [cos(t3), 0, -sin(t3);
     0, 1, 0;
-    -sin(t3), 0, cos(t3);
+    sin(t3), 0, cos(t3);
 ];
 
 rotI = eye(3);
@@ -65,5 +65,5 @@ hold on
 plot3(p(:,1),p(:,2),p(:,3),'-'); % Drawing every line between joints
 x = l1*cos(t1) - l3*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)) + l2*cos(t1)*cos(t2);
 y = l1*sin(t1) - l3*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)) + l2*cos(t2)*sin(t1);
-z = - l3*(cos(t2)*sin(t3) + cos(t3)*sin(t2)) - l2*sin(t2);
+z = l3*(cos(t2)*sin(t3) + cos(t3)*sin(t2)) + l2*sin(t2);
 plot3(x,y,z,'k*');
--- Dexterous/MatlabJacobianUnderstanding/inverse_kinematics.m
+++ Dexterous/MatlabJacobianUnderstanding/inverse_kinematics.m
@@ -6,30 +6,17 @@ l1 = 10; % Length of arms
 l2 = 10;
 l3 = 5;
 t1 = 0; % Angle of joints
-t2 = -pi/4;
-t3 = -pi/4;
+t2 = 1.1;
+t3 = 2.3;
 
-xd = 0;
-yd = 0;
-zd = -0.1;
-
-vel_dot = [0.1; 0; -1.5]; % Vector velocity
+vel_dot = [0; 0.01; 0.01]; % Vector velocity
 dt = 1;
 
-for i = 0:dt:10
+q_list = [];
 
-% Jacobian (Calculated from derivative of forward kinematics)
-J = [[l3*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)) - l1*sin(t1) - l2*cos(t2)*sin(t1), - l3*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2)) - l2*cos(t1)*sin(t2), -l3*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2))];
-[l1*cos(t1) - l3*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)) + l2*cos(t1)*cos(t2), - l3*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2)) - l2*sin(t1)*sin(t2), -l3*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2))];
-[0, -l3*(cos(t2)*cos(t3) - sin(t2)*sin(t3)) - l2*cos(t2), -l3*(cos(t2)*cos(t3) - sin(t2)*sin(t3))]];
+for i = 0:dt:500
 
-theta_dot = inv(J) * vel_dot; % Calculating angular velocities
-
-% Calculating new angles
-% based on previous angles
-t1 = t1 + theta_dot(1)*dt; 
-t2 = t2 + theta_dot(2)*dt;
-t3 = t3 + theta_dot(3)*dt;
+q_list = [q_list; [t1, t2, t3]];
 
 % graph the whole finger huh
 p = [0,0,0];
@@ -37,13 +24,13 @@ rot1 = [cos(t1), -sin(t1), 0;
     sin(t1), cos(t1), 0;
     0, 0, 1;
 ];
-rot2 = [cos(t2), 0, sin(t2);
+rot2 = [cos(t2), 0, -sin(t2);
     0, 1, 0;
-    -sin(t2), 0, cos(t2);
+    sin(t2), 0, cos(t2);
 ];
-rot3 = [cos(t3), 0, sin(t3);
+rot3 = [cos(t3), 0, -sin(t3);
     0, 1, 0;
-    -sin(t3), 0, cos(t3);
+    sin(t3), 0, cos(t3);
 ];
 
 rotI = eye(3);
@@ -73,8 +60,37 @@ plot3(p(:,1),p(:,2),p(:,3),'-');
 
 x = l1*cos(t1) - l3*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)) + l2*cos(t1)*cos(t2);
 y = l1*sin(t1) - l3*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)) + l2*cos(t2)*sin(t1);
-z = - l3*(cos(t2)*sin(t3) + cos(t3)*sin(t2)) - l2*sin(t2);
-[x,y,z]
-plot3(x,y,z,'r*')
+z = l3*(cos(t2)*sin(t3) + cos(t3)*sin(t2)) + l2*sin(t2);
+[x,y,z];
+plot3(x,y,z,'r*');
 hold on
+
+% % Jacobian (Calculated from derivative of forward kinematics)
+J = [[l3*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)) - l1*sin(t1) - l2*cos(t2)*sin(t1), - l3*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2)) - l2*cos(t1)*sin(t2), -l3*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2))];
+[l1*cos(t1) - l3*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)) + l2*cos(t1)*cos(t2), - l3*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2)) - l2*sin(t1)*sin(t2), -l3*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2))];
+[0, l3*(cos(t2)*cos(t3) - sin(t2)*sin(t3)) + l2*cos(t2), l3*(cos(t2)*cos(t3) - sin(t2)*sin(t3))]];
+
+theta_dot = J\vel_dot; % Calculating angular velocities
+
+% Calculating new angles
+% based on previous angles
+t1 = t1 + theta_dot(1)*dt; 
+t2 = t2 + theta_dot(2)*dt;
+t3 = t3 + theta_dot(3)*dt;
+
+% if t1 < -1
+%     t1 = -1;
+% elseif t1 > 1
+%     t1 = 1;
+% end
+% if t2 < 0
+%     t2 = 0;
+% elseif t2 > 2
+%     t2 = 2;
+% end
+% if t3 < 0
+%     t3 = 0;
+% elseif t3 > 2
+%     t3 = 2;
+% end
 end
